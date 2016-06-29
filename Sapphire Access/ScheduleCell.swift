ab1870ab3b5e7f5bca4b4f74db2bb813f53319da
Dhruv Sringari <dhruvy427@gmail.com>
2016-06-29 13:06:31 -0400

    Finishes the UI for the schedule view

---
+++ Sapphire Access/ScheduleCell.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleCell.swift
+//  Sapphire Access
+//
+//  Created by Dhruv Sringari on 6/29/16.
+//  Copyright © 2016 Dhruv Sringari. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleCell: UITableViewCell {
+    @IBOutlet var colorTab: UIView!
+    @IBOutlet var period: UILabel!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var teacher: UILabel!
+    @IBOutlet var room: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
